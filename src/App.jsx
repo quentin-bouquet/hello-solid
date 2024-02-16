85cdf9f6c60c5c0b1a65850bc119b70b3307719b
Quentin Bouquet <quentin.30.bouquet@gmail.com>
2024-02-16 16:10:44 +0100

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello la team!</p>
+    <div>
+        <p class="text-4xl text-green-700 text-center py-20">Hello la team!</p>
+        <p class="text-4xl text-green-700 text-center py-20">Hello la mine!</p>
+
+    </div>
+  
   );
 }
 
